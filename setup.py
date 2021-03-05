cf17c8f76de8d4111eea9434c2163cede01c2417
Gab <gab.despres@gmail.com>
2021-03-04 23:12:57 -0500

    README for release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="raincoat-jackett",
-    version="0.8",
+    version="0.9",
     author="Gabisonfire",
     author_email="gabisonfire@github.com",
     description="Raincoat is a tool to search torrents using Jackett and send them to your client.",
