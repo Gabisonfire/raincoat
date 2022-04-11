0f7307bedffd29670d54555aa82ae4fcd6186753
Gabisonfire <gab.despres@gmail.com>
2022-04-11 19:57:10 -0400

    Fix issue #22 (#24)
    * Possible fix for #22
    
    * Bumped versions
    
    * Fixed missing config in the setup
    
    * Fixed missing config in the setup
    
    * Fix for #22

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="raincoat-jackett",
-    version="1.3",
+    version="1.3.1",
     author="Gabisonfire",
     author_email="gabisonfire@github.com",
     description="Raincoat is a tool to search torrents using Jackett and send them to your client.",
